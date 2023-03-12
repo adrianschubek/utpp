8e4c4f0807ba2c8efc17ea65b2de515d627381bd
Adrian Schubek <adrian.schubek@gmail.com>
2023-03-12 18:08:14 +0100

    rm out

--- out/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.replaceBetween = void 0;
-const replaceBetween = (str, start, end, what) => {
-    return str.substring(0, start) + what + str.substring(end);
-};
-exports.replaceBetween = replaceBetween;
