d4b30db33b14b1cb0a21f8e261e25cb575c9af02
Adrian Schubek <adrian.schubek@gmail.com>
2023-05-08 16:16:00 +0200

    docs: add search

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -74,6 +74,35 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/utpp.png',
+      algolia: {
+        // The application ID provided by Algolia
+        appId: '0VG12FAEM6',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'a90b8ec6a4d93799cc3ea9c91c84af2b',
+  
+        indexName: 'utpp-adriansoftware',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
       navbar: {
         title: 'utpp',
         /* logo: {
